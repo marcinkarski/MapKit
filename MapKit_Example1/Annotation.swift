f1f17c1e7131595b2a52b2b2225a1bce4902d98b
Marcin Karski <karski80@gmail.com>
2018-09-04 17:40:15 +0100

    Custom annotation fail

--- MapKit_Example1/Annotation.swift
+++ MapKit_Example1/Annotation.swift
@@ -4,11 +4,13 @@ final class Annotation: NSObject, MKAnnotation {
     var coordinate: CLLocationCoordinate2D
     var title: String?
     var subtitle: String?
+    var type: String?
     
-    init(coordinate: CLLocationCoordinate2D, title: String?, subtitle: String?) {
+    init(coordinate: CLLocationCoordinate2D, title: String?, subtitle: String?, type: String?) {
         self.coordinate = coordinate
         self.title = title
         self.subtitle = subtitle
+        self.type = type
         
         super.init()
     }
@@ -22,5 +24,19 @@ final class Annotation: NSObject, MKAnnotation {
         }
         self.title = json[2] as? String ?? "No Title"
         self.subtitle = json[3] as? String ?? "No Subtitle"
+        self.type = json[4] as? String ?? "No type"
+    }
+    
+    var markerTintColour: UIColor {
+        switch type {
+        case "Eat":
+            return .red
+        case "See":
+            return .cyan
+        case "Shop":
+            return .blue
+        default:
+            return .orange
+        }
     }
 }
---
+++ MapKit_Example1/AnnotationView.swift
@@ -0,0 +1,10 @@
+import MapKit
+
+class AnnotationView: MKMarkerAnnotationView {
+    override var annotation: MKAnnotation? {
+        willSet {
+            guard let place = newValue as? Annotation else { return }
+            markerTintColor = place.markerTintColour
+        }
+    }
+}
